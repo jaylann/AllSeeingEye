0921ba67b3f7d540ebd0274ba80a0ec697045fd1
jaylann <Justin.Lanfermann@hotmail.com>
2023-08-10 23:12:03 +0200

    added PhoneNumber attribute and associated tests

---
+++ bin/attributes/PhoneNumber.py
@@ -0,0 +1,44 @@
+import phonenumbers
+from bin.attributes.BaseAttribute import BaseAttribute
+
+class PhoneNumber(BaseAttribute):
+    def __init__(self, number, proof, country_context=None):
+        super().__init__(proof)
+        self.number = self.validate_number(number, country_context)
+
+    @property
+    def country_code(self):
+        return self.number.country_code
+
+    @property
+    def local_number(self):
+        return str(self.number.national_number)
+
+    @staticmethod
+    def validate_number(number, country_context):
+        try:
+            parsed_number = phonenumbers.parse(number, country_context)
+            if phonenumbers.is_valid_number(parsed_number):
+                return parsed_number
+        except phonenumbers.phonenumberutil.NumberParseException:
+            pass
+
+        raise ValueError("Invalid phone number format")
+
+    @property
+    def area_code(self):
+        country = phonenumbers.region_code_for_number(self.number)
+        national_number = str(self.number.national_number)
+
+        area_code_length = {
+            'US': 3, 'CA': 3, # North America
+            'GB': 3, 'FR': 2, 'DE': 3, 'IT': 2, # Europe
+            'AU': 1, 'JP': 2, 'KR': 2, # Asia-Pacific
+            'CN': 2, 'IN': 2, 'RU': 3, # Asia
+            'BR': 2, 'MX': 2, 'AR': 2, # Latin America
+            'ZA': 2, 'TR': 2, # Others
+            'SA': 2, # Middle East
+            # Add more if needed
+        }
+
+        return national_number[:area_code_length.get(country, 0)] if country in area_code_length else None
---
+++ tests/attributes/test_phone_number.py
@@ -0,0 +1,67 @@
+import unittest
+import phonenumbers
+
+from bin.attributes.PhoneNumber import PhoneNumber
+from bin.objects.Proof import Proof
+
+class TestPhoneNumber(unittest.TestCase):
+
+    def test_valid_number(self):
+        proof = Proof("Phone")
+        phone = PhoneNumber("+1 650-555-1234", proof)
+        self.assertEqual(phone.country_code, 1)
+        self.assertEqual(phone.local_number, "6505551234")
+        self.assertEqual(phone.area_code, "650")
+
+    def test_invalid_number(self):
+        proof = Proof("Phone")
+        with self.assertRaises(ValueError):
+            PhoneNumber("invalid_number", proof)
+
+    def test_area_code_us(self):
+        proof = Proof("Phone")
+        phone = PhoneNumber("+1 212-555-1234", proof)
+        self.assertEqual(phone.area_code, "212")
+
+    def test_area_code_in(self):
+        proof = Proof("Phone")
+
+        phone = PhoneNumber("+91 22-5555-1234", proof)
+        self.assertEqual(phone.area_code, "22")
+
+    def test_area_code_unknown(self):
+        proof = Proof("Phone")
+        with self.assertRaises(ValueError):
+            PhoneNumber("+123 456-7890", proof)  # Non-existent country code
+
+
+    # Add more specific tests for other G20 countries as needed
+
+    def test_valid_german_number(self):
+        proof = Proof("Phone")
+        phone = PhoneNumber("+491771231231", proof)
+        self.assertEqual(phone.country_code, 49)
+        self.assertEqual(phone.local_number, "1771231231")
+        self.assertEqual(phone.area_code, "177")
+
+    def test_local_number_with_context(self):
+        proof = Proof("Phone")
+        phone = PhoneNumber("01771231231",proof, "DE")  # Parsing with Germany context
+        self.assertEqual(phone.country_code, 49)
+        self.assertEqual(phone.local_number, "1771231231")
+        self.assertEqual(phone.area_code, "177")
+
+    def test_local_number_without_context(self):
+        proof = Proof("Phone")
+        # This test may fail without context, depending on how the class is implemented
+        with self.assertRaises(ValueError):
+            PhoneNumber("01771231231", proof)
+
+    def test_short_number(self):
+        proof = Proof("Phone")
+        # This test may fail if short numbers are considered invalid
+        with self.assertRaises(ValueError):
+            PhoneNumber("12225", proof)
+
+if __name__ == "__main__":
+    unittest.main()
