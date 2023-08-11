39b25462f3a126968d2445c9666c418db01e2d79
jaylann <Justin.Lanfermann@hotmail.com>
2023-08-11 11:03:20 +0200

    updated occupation as well as tests

--- bin/attributes/Occupation.py
+++ bin/attributes/Occupation.py
@@ -32,4 +32,38 @@ class Occupation(BaseAttribute):
     def end_date(self, value):
         self._end_date = convert_to_date(value)    # Validate and convert to datetime
 
-    # Rest of the class as before
+    @property
+    def job_title(self):
+        return self._job_title
+
+    @job_title.setter
+    def job_title(self, value):
+        self._job_title = value
+
+    @property
+    def company_name(self):
+        return self._company_name
+
+    @company_name.setter
+    def company_name(self, value):
+        self._company_name = value
+
+    @property
+    def industry(self):
+        return self._industry
+
+    @industry.setter
+    def industry(self, value):
+        self._industry = value
+
+    @property
+    def years_experience(self):
+        return self._years_experience
+
+    @years_experience.setter
+    def years_experience(self, value):
+        self._years_experience = value
+
+    def __str__(self):
+        return (f"{self.job_title} at {self.company_name}, {self.industry} industry - {self.years_experience} years of "
+                f"experience from {self.start_date} to {self.end_date}")
--- tests/attributes/test_occupation.py
+++ tests/attributes/test_occupation.py
@@ -1,33 +1,42 @@
 import unittest
 from bin.attributes.Occupation import Occupation
 from bin.objects.Proof import Proof
+from datetime import datetime
 
 
 class TestOccupation(unittest.TestCase):
 
     def setUp(self):
         self.proof = Proof("Placeholder")  # You may need to pass the required arguments for the Proof object
-        self.occupation = Occupation("Software Engineer", "TechCorp", "Technology", 5, self.proof)
+        start_date = "2022-01-01"
+        end_date = "2023-01-01"
+        self.occupation = Occupation("Software Engineer", "TechCorp", "Technology", 5, start_date, end_date, self.proof)
 
     def test_initialization(self):
         self.assertEqual(self.occupation.job_title, "Software Engineer")
         self.assertEqual(self.occupation.company_name, "TechCorp")
         self.assertEqual(self.occupation.industry, "Technology")
         self.assertEqual(self.occupation.years_experience, 5)
+        self.assertEqual(self.occupation.start_date, datetime(2022, 1, 1))
+        self.assertEqual(self.occupation.end_date, datetime(2023, 1, 1))
 
     def test_setters(self):
         self.occupation.job_title = "Data Scientist"
         self.occupation.company_name = "DataCorp"
         self.occupation.industry = "Data Science"
         self.occupation.years_experience = 3
+        self.occupation.start_date = "2020-06-01"
+        self.occupation.end_date = "2021-06-01"
 
         self.assertEqual(self.occupation.job_title, "Data Scientist")
         self.assertEqual(self.occupation.company_name, "DataCorp")
         self.assertEqual(self.occupation.industry, "Data Science")
         self.assertEqual(self.occupation.years_experience, 3)
+        self.assertEqual(self.occupation.start_date, datetime(2020, 6, 1))
+        self.assertEqual(self.occupation.end_date, datetime(2021, 6, 1))
 
     def test_string_representation(self):
-        expected_str = "Software Engineer at TechCorp, Technology industry - 5 years of experience"
+        expected_str = "Software Engineer at TechCorp, Technology industry - 5 years of experience from 2022-01-01 00:00:00 to 2023-01-01 00:00:00"
         self.assertEqual(str(self.occupation), expected_str)
 
 
