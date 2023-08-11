ac81dca16d095972e6d6510b41a76499ac637370
jaylann <Justin.Lanfermann@hotmail.com>
2023-08-11 11:43:20 +0200

    made Address arguments optional and added checking to functions to avoid Exceptions

--- bin/attributes/Address.py
+++ bin/attributes/Address.py
@@ -3,7 +3,8 @@ from bin.objects.Proof import Proof
 
 
 class Address(BaseAttribute):
-    def __init__(self, street: str, city: str, state: str, country: str, postal_code: int, proof: Proof):
+    def __init__(self, street: str = None, city: str = None, state: str = None,
+                 country: str = None, postal_code: int = None, proof: Proof = None):
         super().__init__(proof)
         self.street = street
         self.city = city
@@ -13,43 +14,43 @@ class Address(BaseAttribute):
 
     @property
     def street(self):
-        return self._street
+        return self._street if hasattr(self, '_street') else None
 
     @street.setter
     def street(self, value):
-        self._street = value
+        self._street = value if value else None
 
     @property
     def city(self):
-        return self._city
+        return self._city if hasattr(self, '_city') else None
 
     @city.setter
     def city(self, value):
-        self._city = value
+        self._city = value if value else None
 
     @property
     def state(self):
-        return self._state
+        return self._state if hasattr(self, '_state') else None
 
     @state.setter
     def state(self, value):
-        self._state = value
+        self._state = value if value else None
 
     @property
     def country(self):
-        return self._country
+        return self._country if hasattr(self, '_country') else None
 
     @country.setter
     def country(self, value):
-        self._country = value
+        self._country = value if value else None
 
     @property
     def postal_code(self):
-        return self._postal_code
+        return self._postal_code if hasattr(self, '_postal_code') else None
 
     @postal_code.setter
     def postal_code(self, value):
-        self._postal_code = value
+        self._postal_code = value if value else None
 
     def __str__(self):
-        return f"{self.street}, {self.city}, {self.state}, {self.country} - {self.postal_code}"
+        return f"{self.street or ''}, {self.city or ''}, {self.state or ''}, {self.country or ''} - {self.postal_code or ''}"
