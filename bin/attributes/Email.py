18cc6a813719d6f2db7898c02c9f12d55b17fc8d
jaylann <Justin.Lanfermann@hotmail.com>
2023-08-11 10:03:38 +0200

    added Email attribute and associated tests

---
+++ bin/attributes/Email.py
@@ -0,0 +1,31 @@
+import re
+
+from bin.attributes.BaseAttribute import BaseAttribute
+
+
+class Email(BaseAttribute):
+    def __init__(self, email: str, proof):
+        super().__init__(proof)
+        self.email = email
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        if self._is_valid_email(value):
+            self._email = value
+        else:
+            raise ValueError("Invalid email format")
+
+    @property
+    def provider(self):
+        return self._email.split("@")[1].split(".")[0]
+
+    def _is_valid_email(self, email: str) -> bool:
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        return re.match(pattern, email) is not None
+
+    def __str__(self):
+        return self.email
---
+++ tests/attributes/test_email.py
@@ -0,0 +1,43 @@
+import unittest
+
+from bin.attributes.Email import Email
+from bin.objects.Proof import Proof
+
+
+class TestEmail(unittest.TestCase):
+
+    def test_valid_email_creation(self):
+        proof = Proof("Test Proof")
+        email_obj = Email("john.doe@example.com", proof)
+        self.assertEqual(str(email_obj), "john.doe@example.com")
+        self.assertEqual(email_obj.provider, "example")
+
+    def test_invalid_email_creation(self):
+        proof = Proof("Test Proof")
+        with self.assertRaises(ValueError):
+            Email("invalid-email", proof)
+
+    def test_provider_extraction(self):
+        proof = Proof("Test Proof")
+        email_obj = Email("user@google.com", proof)
+        self.assertEqual(email_obj.provider, "google")
+
+        email_obj = Email("user@company.co.uk",proof)
+        self.assertEqual(email_obj.provider, "company")
+
+    def test_email_update(self):
+        proof = Proof("Test Proof")
+        email_obj = Email("john.doe@example.com",proof)
+        email_obj.email = "user@yahoo.com"
+        self.assertEqual(email_obj.email, "user@yahoo.com")
+        self.assertEqual(email_obj.provider, "yahoo")
+
+    def test_invalid_email_update(self):
+        proof = Proof("Test Proof")
+        email_obj = Email("john.doe@example.com",proof)
+        with self.assertRaises(ValueError):
+            email_obj.email = "invalid-email"
+
+
+if __name__ == "__main__":
+    unittest.main()
