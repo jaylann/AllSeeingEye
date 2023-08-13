31e547c4265534989f0cb242ec43e01ba78cc89c
jaylann <Justin.Lanfermann@hotmail.com>
2023-08-13 16:24:56 +0200

    optimized and refactored add function

--- main.py
+++ main.py
@@ -29,6 +29,41 @@ def person():
 
 cli.add_command(person)
 
+from typing import Dict, Any
+
+
+def get_attribute_input(attribute_name: str, required: bool = False) -> Dict[str, Any]:
+    prompt_suffix = ('proof', 'Enter proof reference number', str)
+    prompts = {
+        'address': [('street', 'Enter street', str), ('city', 'Enter city', str),
+                    ('postal_code', 'Enter zip code', int),
+                    ('state', 'Enter state', str), ('country', 'Enter country', str)],
+        'dob': [('dob', 'Enter date of birth (YYYY-MM-DD)', str)],
+        'email': [('email', 'Enter email', str)],
+        'gender': [('gender', 'Enter gender (Male/Female/Other)', str)],
+        'name': [('name', 'Enter first name', str), ('surname', 'Enter last name', str),
+                 ('middlenames', 'Enter middle names (comma separated)', str)],
+        'nationality': [('nationality', 'Enter nationality', str)],
+        'occupation': [('job_title', 'Enter job title', str), ('company_name', 'Enter company name', str),
+                       ('industry', 'Enter industry', str), ('years_of_experience', 'Enter years of experience', int),
+                       ('start_date', 'Enter start date (YYYY-MM-DD)', str),
+                       ('end_date', 'Enter end date (YYYY-MM-DD)', str)],
+        'phone_number': [('number', 'Enter phone number', str),
+                         ('country_context', 'Enter country context (optional)', str)],
+        'relationship_status': [('relationship_status', 'Enter relationship status', str)],
+    }
+
+    # Appending 'proof' to each prompt
+    for key in prompts:
+        prompts[key].append(prompt_suffix)
+
+    inputs = {
+        param_name: get_user_input(prompt, input_type=input_type,
+                                   required=(required if param_name != 'proof' else False))
+        for param_name, prompt, input_type in prompts[attribute_name]
+    }
+    return inputs
+
 
 @person.command()
 @click.option('--address', is_flag=True, help='Address of the person.')
@@ -40,77 +75,27 @@ cli.add_command(person)
 @click.option('--occupation', is_flag=True, help='Occupation of the person.')
 @click.option('--phone-number', is_flag=True, help='Phone number of the person.')
 @click.option('--relationship-status', is_flag=True, help='Relationship status of the person.')
-def add(address, dob, email, gender, name, nationality, occupation, phone_number, relationship_status):
+def add(address: bool, dob: bool, email: bool, gender: bool, name: bool, nationality: bool, occupation: bool,
+        phone_number: bool, relationship_status: bool):
     """Add a new person with optional attributes."""
+    attribute_mapping = {
+        'address': (address, Address),
+        'dob': (dob, DOB),
+        'email': (email, Email),
+        'gender': (gender, Gender),
+        'name': (name, Name),
+        'nationality': (nationality, Nationality),
+        'occupation': (occupation, Occupation),
+        'phone_number': (phone_number, PhoneNumber),
+        'relationship_status': (relationship_status, RelationshipStatus),
+    }
 
-    address_obj = dob_obj = email_obj = gender_obj = name_obj = nationality_obj = occupation_obj = phone_number_obj = relationship_status_obj = None
+    attributes_obj = {
+        attribute: cls(**get_attribute_input(attribute, required=(attribute in ['dob', 'email', 'gender'])))
+        for attribute, (flag, cls) in attribute_mapping.items() if flag
+    }
 
-    if address:
-        street = get_user_input('Enter street')
-        city = get_user_input('Enter city')
-        postal_code = get_user_input('Enter zip code', input_type=int)
-        state = get_user_input('Enter state')
-        country = get_user_input("Enter country")
-        proof = get_user_input('Enter proof reference number')
-        address_obj = Address(street=street, city=city, state=state, country=country, postal_code=postal_code,
-                              proof=proof)
-
-    if dob:
-        dob_value = get_user_input('Enter date of birth (YYYY-MM-DD)', required=True)
-        proof = get_user_input('Enter proof reference number')
-        dob_obj = DOB(dob=dob_value, proof=proof)
-
-    if email:
-        email_value = get_user_input('Enter email', required=True)
-        proof = get_user_input('Enter proof reference number')
-        email_obj = Email(email=email_value, proof=proof)
-
-    if gender:
-        gender_value = get_user_input('Enter gender (Male/Female/Other)', required=True)
-        proof = get_user_input('Enter proof reference number')
-        gender_obj = Gender(gender=gender_value, proof=proof)
-
-    if name:
-        first_name = get_user_input('Enter first name')
-        last_name = get_user_input('Enter last name')
-        middle_names = get_user_input('Enter middle names (comma separated)')
-        proof = get_user_input('Enter proof reference number')
-        name_obj = Name(name=first_name, surname=last_name, middlenames=middle_names, proof=proof)
-
-    if nationality:
-        nationality_value = get_user_input('Enter nationality', required=True)
-        proof = get_user_input('Enter proof reference number')
-        nationality_obj = Nationality(country=nationality_value, proof=proof)
-
-    if occupation:
-        job_title = get_user_input('Enter job title')
-        company_name = get_user_input('Enter company name')
-        industry = get_user_input('Enter industry')
-        years_experience = get_user_input('Enter years of experience', input_type=int) or None
-        start_date = get_user_input('Enter start date (YYYY-MM-DD)')
-        end_date = get_user_input('Enter end date (YYYY-MM-DD)')
-        proof = get_user_input('Enter proof reference number')
-        occupation_obj = Occupation(job_title=job_title, company_name=company_name, industry=industry,
-                                    years_experience=years_experience, start_date=start_date, end_date=end_date,
-                                    proof=proof)
-
-    if phone_number:
-        phone_number_value = get_user_input('Enter phone number', required=True)
-        country_context = get_user_input("Enter country context (optional)")
-        proof = get_user_input('Enter proof reference number')
-        print(phone_number_value)
-        phone_number_obj = PhoneNumber(number=phone_number_value, country_context=country_context, proof=proof)
-
-    if relationship_status:
-        relationship_status_value = get_user_input('Enter relationship status', required=True)
-        proof = get_user_input('Enter proof reference number')
-        relationship_status_obj = RelationshipStatus(relationship_status=relationship_status_value, proof=proof)
-
-    uid = database.add_person(Person(
-        dob=dob_obj, name=name_obj, address=address_obj, phone_number=phone_number_obj, nationality=nationality_obj,
-        email=email_obj, gender=gender_obj, occupation=occupation_obj,
-        relationship_status=relationship_status_obj
-    ))
+    uid = database.add_person(Person(**attributes_obj))
     click.echo(f"Person added successfully with Unique ID: {uid}")
 
 
