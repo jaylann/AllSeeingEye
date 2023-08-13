00f7b52cf95e8b1978a9cefbcd25a62b97d6787c
jaylann <Justin.Lanfermann@hotmail.com>
2023-08-13 20:18:30 +0200

    added more detailed test cases

--- tests/attributes/test_email.py
+++ tests/attributes/test_email.py
@@ -38,6 +38,37 @@ class TestEmail(unittest.TestCase):
         with self.assertRaises(ValueError):
             email_obj.email = "invalid-email"
 
+    def test_initialization_with_empty_email(self):
+        proof = Proof("Test Proof")
+        with self.assertRaises(ValueError):
+            Email("", proof)
+
+    def test_emails_with_uncommon_formats(self):
+        proof = Proof("Test Proof")
+        email_obj = Email("user@sub.example.com", proof)
+        self.assertEqual(email_obj.provider, "sub")
+
+        email_obj = Email("user@subdomain.company.co.uk", proof)
+        self.assertEqual(email_obj.provider, "subdomain")
+
+    def test_email_with_multiple_errors(self):
+        proof = Proof("Test Proof")
+        with self.assertRaises(ValueError):
+            Email("@missing-user-part.com", proof)
+        with self.assertRaises(ValueError):
+            Email("user@.missing-tld", proof)
+        with self.assertRaises(ValueError):
+            Email("user@missing-tld.", proof)
+
+    def test_dict_representation(self):
+        proof = Proof("Test Proof")
+        email_obj = Email("john.doe@example.com", proof)
+        expected_dict = {
+            'email': "john.doe@example.com",
+            'proof': [proof.__dict__()]  # Adjust as needed based on Proof's __dict__ implementation
+        }
+        self.assertEqual(email_obj.__dict__(), expected_dict)
+
 
 if __name__ == "__main__":
     unittest.main()
