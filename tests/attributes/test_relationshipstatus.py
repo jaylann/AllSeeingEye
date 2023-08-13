09bccc0bd81effdab64d45831bc942a87ed92add
jaylann <Justin.Lanfermann@hotmail.com>
2023-08-13 21:22:41 +0200

    added more detailed test cases.

--- tests/attributes/test_relationshipstatus.py
+++ tests/attributes/test_relationshipstatus.py
@@ -26,6 +26,31 @@ class TestRelationshipStatus(unittest.TestCase):
         status = RelationshipStatus('Married', self.proof)
         self.assertEqual(str(status), 'Relationship Status: Married')
 
+    def test_initialization_without_proof(self):
+        status = RelationshipStatus('Single')
+        self.assertEqual(status.status, 'Single')
+
+    def test_case_sensitivity(self):
+        # This test assumes that the status is case-sensitive. If it is not, the test should be adjusted accordingly.
+        with self.assertRaises(ValueError):
+            RelationshipStatus('single', self.proof)
+
+    def test_dictionary_representation(self):
+        status = RelationshipStatus('Married', self.proof)
+        expected_dict = {
+            'status': 'Married',
+            'proof': [self.proof.__dict__()]  # Assuming the Proof object has a __dict__ method
+        }
+        self.assertEqual(status.__dict__(), expected_dict)
+
+    def test_dictionary_representation_without_proof(self):
+        status = RelationshipStatus('Married')
+        expected_dict = {
+            'status': 'Married',
+            'proof': None
+        }
+        self.assertEqual(status.__dict__(), expected_dict)
+
 
 if __name__ == '__main__':
     unittest.main()
