d0743f412ca153e6940ddec1671a8284d97f6d84
jaylann <Justin.Lanfermann@hotmail.com>
2023-08-13 20:01:00 +0200

    refactored and added documentation.

--- bin/attributes/Nationality.py
+++ bin/attributes/Nationality.py
@@ -1,25 +1,50 @@
+from typing import Optional
 from bin.attributes.BaseAttribute import BaseAttribute
 from bin.objects.Proof import Proof
 
 
 class Nationality(BaseAttribute):
-    def __init__(self, country: str, proof: Proof = None):
+    """
+    Nationality class that extends BaseAttribute.
+    It represents a person's nationality, identified by their country.
+
+    Attributes:
+        country (str): The country representing the nationality.
+    """
+
+    def __init__(self, country: str, proof: Optional[Proof] = None):
+        """
+        Initializes the Nationality object.
+
+        Args:
+            country (str): The country representing the nationality.
+            proof (Optional[Proof], default=None): Proof associated with the nationality.
+        """
         super().__init__(proof)
         self.country = country
 
     @property
-    def country(self):
+    def country(self) -> str:
+        """Returns the country representing the nationality."""
         return self._country
 
     @country.setter
-    def country(self, value):
+    def country(self, value: str):
+        """Sets the country representing the nationality.
+
+        Args:
+            value (str): The country to set.
+        """
         self._country = value
 
-    def __str__(self):
-        return f"{self.country}"
+    def __str__(self) -> str:
+        """Returns the string representation of the nationality, which is the country."""
+        return self.country
 
-    def __dict__(self):
+    def __dict__(self) -> dict:
+        """Returns a dictionary representation of the Nationality object, including proofs if available."""
         return {
             'country': self._country,
-            'proof': [proof.__dict__() for proof in self.proof] if self.proof else None # Assuming proof is defined in the BaseAttribute class
+            # Convert proof objects to dictionaries if proof attribute is defined in the BaseAttribute class
+            'proof': [proof.__dict__() for proof in self.proof] if self.proof else None
         }
